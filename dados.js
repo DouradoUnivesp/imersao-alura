f49aa0ae5ab2d9829ec57af58641021169f9d929
DouradoUnivesp <69279677+DouradoUnivesp@users.noreply.github.com>
2024-09-05 09:56:18 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        nome : "Python",
+        cursos : "1. Python.org, 2. Codecademy, 3. Google's Python Class, 4. Microsoft Learn, 5. Coursera",
+        link : "https://pt.wikipedia.org/wiki/Python"
+    },
+    {
+        nome: "C#",
+        cursos: "1. Microsoft Learn, 2. Udemy, 3. Coursera, 4. Pluralsight",
+        link: "https://pt.wikipedia.org/wiki/C_sharp"
+    },
+    {
+        nome: "Java",
+        cursos: "1. Oracle Academy, 2. Udemy, 3. Coursera, 4. Edureka",
+        link: "https://pt.wikipedia.org/wiki/Java_(linguagem_de_programação)"
+    }
+];
